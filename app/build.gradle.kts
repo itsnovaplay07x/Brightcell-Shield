82d8644baeb13c8b9998aa9a066962e54a7f91f0
itsnovaplay07x <parshuram843121@gmail.com>
2026-09-06 04:34:57 +0530

    Add build.gradle.kts for Android application setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.brightcell.shield"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.brightcell.shield"
+        minSdk = 26
+        targetSdk = 35
+
+        versionCode = 11
+        versionName = "11.0"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+}
