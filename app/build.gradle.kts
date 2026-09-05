d333c95aba45ce923f1b134ddfa76ae728e53a35
itsnovaplay07x <parshuram843121@gmail.com>
2026-09-06 04:57:15 +0530

    Update build.gradle.kts for Kotlin and Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,23 +1,64 @@
 plugins {
+
     alias(libs.plugins.android.application)
+
     alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
+
     namespace = "com.brightcell.shield"
+
     compileSdk = 35
 
     defaultConfig {
+
         applicationId = "com.brightcell.shield"
+
         minSdk = 26
+
         targetSdk = 35
 
         versionCode = 11
+
         versionName = "11.0"
     }
 
+    buildTypes {
+
+        release {
+
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+
+        jvmTarget = "17"
+    }
+
     buildFeatures {
+
         compose = true
+
         buildConfig = true
     }
 }
@@ -25,15 +66,26 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
 
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(
+        platform(libs.androidx.compose.bom)
+    )
 
     implementation(libs.androidx.ui)
+
     implementation(libs.androidx.ui.graphics)
+
     implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.androidx.material3)
 
     implementation(libs.kotlinx.coroutines.android)
+
+    debugImplementation(
+        libs.androidx.ui.tooling
+    )
 }
